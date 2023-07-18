aee7dd564ea44b026280bf6eb151e9a660b1a069
Zilola-Nazarova <61951420+Zilola-Nazarova@users.noreply.github.com>
2023-07-19 04:22:17 +0500

    Write more tests for capitalize function, tests are passing so the function doesn't need any changes

--- capitalize.test.js
+++ capitalize.test.js
@@ -10,3 +10,36 @@ test('lowercase string', () => {
   // Assert
   expect(capitalizedString).toBe('Zilola');
 });
+
+test('uppercase string', () => {
+  // Arrange
+  const inputString = 'ZILola';
+
+  // Act
+  const capitalizedString = capitalize(inputString);
+
+  // Assert
+  expect(capitalizedString).toBe('ZILola');
+});
+
+test('string starting with integer', () => {
+  // Arrange
+  const inputString = '1zilola';
+
+  // Act
+  const capitalizedString = capitalize(inputString);
+
+  // Assert
+  expect(capitalizedString).toBe('1zilola');
+});
+
+test('integer input', () => {
+  // Arrange
+  const inputString = '53';
+
+  // Act
+  const capitalizedString = capitalize(inputString);
+
+  // Assert
+  expect(capitalizedString).toBe('53');
+});
